c6832f54fa70450950da1da52188aec7dfd09268
이준협 <121070166+Ljunhyeob@users.noreply.github.com>
2023-01-03 22:25:28 +0900

    Add files via upload

---
+++ baekjoon1008/baekjoon1008/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  baekjoon1008
+//
+//  Created by 이준협 on 2023/01/03.
+//
+
+import Foundation
+
+let line = readLine()!
+let lineArr = line.components(separatedBy: " ")
+let a = Double(lineArr[0])!
+let b = Double(lineArr[1])!
+print(a/b)
